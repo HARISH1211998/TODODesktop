e4b4c89fb05437962deb9b1f422c13e027f584fe
HarishankarVj12 <harishankar@ix.cumberlandlabs.io>
2023-07-23 13:05:17 +0530

    Added an Web project in python

---
+++ function.py
@@ -0,0 +1,9 @@
+filepath1 = "todos.txt"
+def getTodos(filepath=filepath1):
+    with open(filepath, 'r') as file:
+        todos = file.readlines()
+    return todos
+
+def putTodos(todos, filepath=filepath1):
+    with open(filepath, 'w') as file:
+        file.writelines(todos)
---
+++ web.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.title("MY website First web port")
